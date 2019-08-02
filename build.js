f8e56b3202a29741396b735040886ef303c816a8
Steve Shaffer <SteveShaffer@users.noreply.github.com>
2019-08-02 15:18:36 -0700

    VTN-22754 Upgrade now to v2 (#123)
    Well that was easier than I thought it'd be. :)

--- build.js
+++ build.js
@@ -1,8 +1,23 @@
 const { readFileSync, writeFileSync } = require('fs');
 
-const buildDirectory = `build-${process.env.ENVIRONMENT || 'local'}`;
+const { exec, cp, rm, mkdir } = require('shelljs');
+
+const buildDirectory = process.env.OUTPUT_DIRECTORY || `build-${process.env.ENVIRONMENT || 'local'}`;
+
+// Run schemagen.sh
+// TODO: Refactor into node.js (if it's even needed anymore)
+if (exec('./schemagen.sh').code !== 0) {
+  shell.echo('Error running schemagen');
+  shell.exit(1);
+}
+
+// Copy the source code to public directory
+rm('-rf', buildDirectory);
+mkdir(buildDirectory);
+cp('-r', 'docs/*', buildDirectory);
+
+// Inject config into index
 const config = JSON.parse(readFileSync('config.json', 'utf8'));
 const indexFileLocation = `${buildDirectory}/index.html`;
 const indexContents = readFileSync(indexFileLocation, 'utf8');
-
 writeFileSync(indexFileLocation, indexContents.replace('@@apiRoot@@', config.apiRoot));
